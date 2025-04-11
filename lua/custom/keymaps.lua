971736f3edf2b229b31c629be4768ba6fe1363e1
saran <saran.v@codingmart.com>
2025-04-11 12:41:40 +0530

    added diagnostic keymap

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -19,3 +19,4 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower win
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 vim.keymap.set("n", "<leader>bd", "<cmd>%bd|e#<CR>")
+vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float, { desc = "expand diagnostic or current line" })
